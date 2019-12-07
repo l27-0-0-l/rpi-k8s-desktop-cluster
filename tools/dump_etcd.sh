7ba27e225a31a55dc69318fb543864f8def16a48
none <none@localhost.localdomain>
2019-12-07 20:41:57 +0100

    improving dump tools

--- tools/dump_etcd.sh
+++ tools/dump_etcd.sh
@@ -1,11 +1,44 @@
 #!/bin/bash
 
-DATE=$(date +%Y-%m-%d_%H-%M-%s)
-mkdir DUMP_ETCD_$DATE
-cd DUMP_ETCD_$DATE
+if [ -z "$1" ] ; then
+   kubectl config get-contexts
+   exit 1
+fi
 
-kubectl -n kube-system exec -it etcd-rpi3-0 -- /bin/sh -c 'ETCDCTL_API=3 etcdctl --endpoints https://127.0.0.1:2379 --cacert /etc/kubernetes/pki/etcd/ca.crt --key /etc/kubernetes/pki/etcd/server.key --cert /etc/kubernetes/pki/etcd/server.crt get "" --prefix=true -w json'  > etcd_dump.json
+CTX_ARG=${1:-}
+CONTEXT=" --context $CTX_ARG "
+DATE=$(date +%Y-%m-%d_%H-%M-%S)
+mkdir DUMP_ETCD_${CTX_ARG}_$DATE
+cd DUMP_ETCD_${CTX_ARG}_$DATE
+
+kubectl $CONTEXT -n kube-system exec -it etcd-rpi3-0 -- /bin/sh -c 'ETCDCTL_API=3 etcdctl --endpoints https://127.0.0.1:2379 --cacert /etc/kubernetes/pki/etcd/ca.crt --key /etc/kubernetes/pki/etcd/server.key --cert /etc/kubernetes/pki/etcd/server.crt get "" --prefix=true -w json'  > etcd_dump.json
 
 cat etcd_dump.json | jq -r .kvs[].key  > keys
 
 for i in $(cat keys) ; do echo -n $i|base64 -d ; echo ; done > dump_all
+
+TOTAL=$(cat etcd_dump.json | jq .count)
+CNT=0
+DUMP="$PWD/etcd_dump.json"
+mkdir CONTENT
+cd CONTENT
+while [ $CNT -lt $TOTAL ] ; do
+   KEY=$(cat "$DUMP" | jq -r ".kvs[$CNT].key"|base64 --decode)
+   mkdir -p ".$KEY"
+   cat "$DUMP" | jq -r ".kvs[$CNT].key" | base64 --decode > ".$KEY"/key
+   cat "$DUMP" | jq -r ".kvs[$CNT].value" | base64 --decode > ".$KEY"/value
+   echo $CNT > ".$KEY"/num
+   mkdir -p BYCNT
+   cd BYCNT
+   mkdir $CNT
+   cd $CNT
+   cat "$DUMP" | jq -r ".kvs[$CNT].key" | base64 --decode > key 
+   cat "$DUMP" | jq -r ".kvs[$CNT].value" | base64 --decode > value
+   FILETYPE=$(file -b value)
+   if [ "$FILETYPE" == "JSON data" ] ; then
+       cp value json_value
+   fi
+   cd ..
+   cd ..
+   CNT=$((CNT+1))
+done
--- tools/get_all_objects.sh
+++ tools/get_all_objects.sh
@@ -1,27 +1,64 @@
 #!/bin/bash
 
-NAMESPACES=$(kubectl get namespaces -o jsonpath="{.items[*].metadata.name}")
+if [ -z "$1" ] ; then
+   kubectl config get-contexts
+   exit 1
+fi
 
-NAMESPACED_TRUE=$(kubectl api-resources --namespaced=true --verbs=get | grep -v ^NAME | awk '{print $1}')
-NAMESPACED_FALSE=$(kubectl api-resources --namespaced=false --verbs=get | grep -v ^NAME | awk '{print $1}')
+CTX_ARG=${1:-}
+CONTEXT=" --context $CTX_ARG "
+NAMESPACES=$(kubectl $CONTEXT get namespaces -o jsonpath="{.items[*].metadata.name}")
 
-echo NAMESPACED $NAMESPACED_TRUE
-echo SOLO $NAMESPACED_FALSE
+NAMESPACED_TRUE=$(kubectl $CONTEXT api-resources --namespaced=true --verbs=get | grep -v ^NAME | awk '{print $1}' | sort | uniq)
+NAMESPACED_FALSE=$(kubectl $CONTEXT api-resources --namespaced=false --verbs=get | grep -v ^NAME | awk '{print $1}' | sort | uniq)
 
-DATE=$(date +%Y-%m-%d_%H-%M-%s)
-mkdir DUMP_KUBE_$DATE
-cd DUMP_KUBE_$DATE
+DATE=$(date +%Y-%m-%d_%H-%M-%S)
+mkdir "DUMP_KUBE_${CTX_ARG}_$DATE"
+cd "DUMP_KUBE_${CTX_ARG}_$DATE"
 
-for i in $NAMESPACED_FALSE ; do 
-    kubectl get $i > $i.txt
+for j in $NAMESPACED_FALSE ; do
+    kubectl $CONTEXT get "$j" > "$j.txt" 2> /dev/null
+    if [ ! -s "$j.txt" ] ; then
+        rm -f "$j.txt"
+    else
+        kubectl $CONTEXT get "$j" -o yaml > "$j.yaml" 2> /dev/null
+        if [ "events" != "$j" ] ; then
+            OBJECTS=$(cat "$j.txt"|grep -v ^NAME|awk '{print $1}')
+            mkdir "$j"
+            cd "$j"
+            for k in $OBJECTS ; do 
+                kubectl $CONTEXT get "$j" "$k" -o yaml > "$j.$k.yaml" 2> /dev/null
+            done
+            cd ..
+            ls "$j" > "$j.lst"
+        fi
+    fi
 done
 
+mkdir _NAMESPACES
+cd _NAMESPACES
 for i in $NAMESPACES ; do
-    mkdir $i
-    ( cd $i
+    mkdir "$i"
+    cd "$i"
         for j in $NAMESPACED_TRUE ; do
-            kubectl -n $i get $j > $j.txt
+            kubectl $CONTEXT -n "$i" get "$j" > "$j.txt" 2> /dev/null
+            if [ ! -s "$j.txt" ] ; then
+                rm -f "$j.txt"
+            else
+                kubectl $CONTEXT -n "$i" get "$j" -o yaml > "$j.yaml" 2> /dev/null
+                if [ "events" != "$j" ] ; then
+                    OBJECTS=$(cat "$j.txt"|grep -v ^NAME|awk '{print $1}')
+                    mkdir "$j"
+                    cd "$j"
+                    for k in $OBJECTS ; do 
+                        kubectl $CONTEXT -n "$i" get "$j" "$k" -o yaml > "$j.$k.yaml" 2> /dev/null
+                    done
+                    cd ..
+                    ls "$j" > "$j.lst"
+                fi
+ 
+            fi
          done
-    )
+    cd ..
+    ls "$i" > "$i.lst"
 done
-
